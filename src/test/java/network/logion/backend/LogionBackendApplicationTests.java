da9b5b99dcadcdbf4b6bfae26f3b90c3bb82924d
Benoit Devos <benoit@logion.network>
2021-05-10 12:28:25 +0200

    feat: Spring Boot setup (Maven, Java 11, Spring Web)

---
+++ src/test/java/network/logion/backend/LogionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package network.logion.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
