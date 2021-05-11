90971535122c81e2b8745bd2c728d591f5aad8e3
Gerard Dethier <gerard@logion.network>
2021-05-11 11:23:27 +0200

    feat: add subkey wrapper.
    logion-network/logion-internal#20

---
+++ src/main/java/logion/backend/subkey/SubkeyWrapper.java
@@ -0,0 +1,93 @@
+package logion.backend.subkey;
+
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+public class SubkeyWrapper {
+
+    public ExpectingAddress verify(String signature) {
+        requireNonNull(signature);
+        var verifier = new ExpectingAddress();
+        verifier.signature = signature;
+        return verifier;
+    }
+
+    public class ExpectingAddress {
+
+        private String signature;
+
+        public ExpectingMessage withSs58Address(String address) {
+            var expectsMessage = new ExpectingMessage();
+            expectsMessage.address = address;
+            return expectsMessage;
+        }
+
+        public class ExpectingMessage {
+
+            private String address;
+
+            public boolean withMessage(String message) {
+                var command = buildSubkeyCommand();
+                command.add("verify");
+                command.add(signature);
+                command.add(address);
+                return callSubkeyWithInput(message, command).exitValue() == 0;
+            }
+
+            private ExpectingMessage() {
+
+            }
+        }
+
+        private ExpectingAddress() {
+
+        }
+    }
+
+    private List<String> buildSubkeyCommand() {
+        var command = new ArrayList<String>();
+        command.add(subkey);
+        return command;
+    }
+
+    private String subkey;
+
+    private Process callSubkeyWithInput(String input, List<String> command) {
+        try {
+            var process = new ProcessBuilder(command.toArray(new String[command.size()])).start();
+            var writer = new OutputStreamWriter(process.getOutputStream(), Charset.forName("UTF-8"));
+            writer.write(input);
+            writer.close();
+            process.waitFor();
+            return process;
+        } catch (Exception e) {
+            throw new SubkeyException("Failed to execute subkey", e);
+        }
+    }
+
+    public static SubkeyWrapper defaultInstance() {
+        return new SubkeyWrapper.Builder().build();
+    }
+
+    public static class Builder {
+
+        public SubkeyWrapper build() {
+            return wrapper;
+        }
+
+        private SubkeyWrapper wrapper = new SubkeyWrapper();
+
+        public Builder withSubkey(String subkey) {
+            wrapper.subkey = subkey;
+            return this;
+        }
+    }
+
+    private SubkeyWrapper() {
+        subkey = "subkey";
+    }
+}
---
+++ src/test/java/logion/backend/subkey/SubkeyWrapperTest.java
@@ -0,0 +1,59 @@
+package logion.backend.subkey;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SubkeyWrapperTest {
+
+    @Test
+    public void verifiesRightSignature() {
+        givenAddress("5Gv8YYFu8H1btvmrJy9FjjAWfb99wrhV3uhPFoNEr918utyR");
+        givenMessage("test message\n");
+        givenSignature("22f91b41ba12f8663ddce26bfc90dbfe6a51683fd3782ad679ab2a5fdbe7d44c2a119f22c74eea22555e5483eb7f42b828f189a38379d59c3b607d2461f0858e");
+        whenVerifying();
+        thenVerificationResultIs(true);
+    }
+
+    private void givenAddress(String address) {
+        this.address = address;
+    }
+
+    private String address;
+
+    private void givenMessage(String message) {
+        this.message = message;
+    }
+
+    private String message;
+
+    private void givenSignature(String signature) {
+        this.signature = signature;
+    }
+
+    private String signature;
+
+    private void whenVerifying() {
+        var subkey = SubkeyWrapper.defaultInstance();
+        verificationResult = subkey
+                .verify(signature)
+                .withSs58Address(address)
+                .withMessage(message);
+    }
+
+    private boolean verificationResult;
+
+    private void thenVerificationResultIs(boolean expected) {
+        assertThat(verificationResult, is(expected));
+    }
+
+    @Test
+    public void rejectsWrongSignature() {
+        givenAddress("5Gv8YYFu8H1btvmrJy9FjjAWfb99wrhV3uhPFoNEr918utyR");
+        givenMessage("another message\n");
+        givenSignature("22f91b41ba12f8663ddce26bfc90dbfe6a51683fd3782ad679ab2a5fdbe7d44c2a119f22c74eea22555e5483eb7f42b828f189a38379d59c3b607d2461f0858e");
+        whenVerifying();
+        thenVerificationResultIs(false);
+    }
+}
