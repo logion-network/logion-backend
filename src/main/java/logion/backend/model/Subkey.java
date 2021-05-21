75747c5cc68c0d40421f3f48b96c107364220a97
Gerard Dethier <gerard@logion.network>
2021-05-21 09:11:31 +0200

    feat: read subkey command from properties.

--- src/main/java/logion/backend/model/Subkey.java
+++ src/main/java/logion/backend/model/Subkey.java
@@ -1,12 +1,32 @@
 package logion.backend.model;
 
 import logion.backend.subkey.SubkeyWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-public class Subkey {
+public class Subkey implements InitializingBean {
 
     public SubkeyWrapper.ExpectingAddress verify(String signature) {
-        return SubkeyWrapper.defaultInstance().verify(signature);
+        return newWrapper().verify(signature);
     }
+
+    private SubkeyWrapper newWrapper() {
+        return new SubkeyWrapper.Builder()
+                .withSubkey(subkeyPath)
+                .build();
+    }
+
+    @Value("${logion.subkey.command:subkey}")
+    private String subkeyPath;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        logger.info("Subkey command: {}", subkeyPath);
+    }
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
 }
