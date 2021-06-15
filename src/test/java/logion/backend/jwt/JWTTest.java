1842540fb570f4e3f09545cf86a6ec8d0e185440
Benoit Devos <benoit@logion.network>
2021-06-15 11:49:54 +0200

    chore: revamp JWTTest
    logion-network/logion-internal#71

--- src/test/java/logion/backend/jwt/JWTTest.java
+++ src/test/java/logion/backend/jwt/JWTTest.java
@@ -15,20 +15,19 @@ import static org.hamcrest.Matchers.equalTo;
 class JWTTest {
 
     private static final String SECRET = "secret-key-that-no-one-could-possibly-know";
-    private static final String SESSION_ID = "9f2a616b-4791-4fd4-a573-51b3f3e897b0";
-    private static final String TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkZXYubG9naW9uLm5ldHdvcmsiLCJzdWIiOiI1R3J3dmFFRjV6WGIyNkZ6OXJjUXBEV1M1N0N0RVJIcE5laFhDUGNOb0hHS3V0UVkiLCJqdGkiOiI5ZjJhNjE2Yi00NzkxLTRmZDQtYTU3My01MWIzZjNlODk3YjAiLCJpYXQiOjE2MjM2NzQwOTksImV4cCI6MTgyMzY3NDA5OSwibGVnYWxPZmZpY2VyIjp0cnVlfQ.BkexoDdhnpBYN1x07F2aCANjaIyHRVPSLxjF8oUTe_Q";
+    private static final String TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJkZXYubG9naW9uLm5ldHdvcmsiLCJzdWIiOiI1R3J3dmFFRjV6WGIyNkZ6OXJjUXBEV1M1N0N0RVJIcE5laFhDUGNOb0hHS3V0UVkiLCJpYXQiOjE2MjM2NzQwOTksImV4cCI6MTgyMzY3NDA5OSwibGVnYWxPZmZpY2VyIjp0cnVlfQ.DYfQDAugVG7Sp3IBtwK_1rN_eHlr4-7axU4KTDvVCho";
     private static final String ISSUER = "dev.logion.network";
+    private static final String ROLE_LEGAL_OFFICER = "legalOfficer";
 
     @Test
-    void encode() throws Exception {
+    void encode() {
         SecretKey secretKey = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
         String encoded = Jwts.builder()
                 .setIssuer(ISSUER)
                 .setSubject(DefaultAddresses.ALICE.getRawValue())
-                .setId(SESSION_ID)
                 .setIssuedAt(Date.from(Instant.ofEpochSecond(1623674099)))
                 .setExpiration(Date.from(Instant.ofEpochSecond(1823674099)))
-                .claim("legalOfficer", true)
+                .claim(ROLE_LEGAL_OFFICER, true)
                 .signWith(secretKey)
                 .compact();
         assertThat(encoded, equalTo(TOKEN));
@@ -37,16 +36,15 @@ class JWTTest {
     @Test
     void decode() {
         SecretKey secretKey = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
-
         var jwt = Jwts.parserBuilder()
                 .setSigningKey(secretKey)
                 .requireIssuer(ISSUER)
                 .requireSubject(DefaultAddresses.ALICE.getRawValue())
-                .require("legalOfficer", true)
+                .require(ROLE_LEGAL_OFFICER, true)
                 .build()
                 .parseClaimsJws(TOKEN);
         assertThat(jwt.getBody().getIssuer(), equalTo(ISSUER));
         assertThat(jwt.getBody().getSubject(), equalTo(DefaultAddresses.ALICE.getRawValue()));
-        assertThat(jwt.getBody().get("legalOfficer", Boolean.class), equalTo(true));
+        assertThat(jwt.getBody().get(ROLE_LEGAL_OFFICER, Boolean.class), equalTo(true));
     }
 }
